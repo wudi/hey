006704d9939a7f1d44d3608ef93249c2a91c56ab
wudi <hi@wudi.io>
2025-09-18 12:31:54 +0800

    Remove useless files

--- stdlib/stdlib.go
+++
@@ -1,8 +0,0 @@
-package stdlib
-
-// InitializeUnifiedClasses registers standard library classes with the unified
-// registry. The current implementation is a placeholder; comprehensive class
-// metadata will be added as the runtime grows more capable.
-func InitializeUnifiedClasses() error {
-	return nil
-}
